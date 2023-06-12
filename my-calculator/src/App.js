303f797ae94ff51479cf11e7716a6b8315d1cfa6
shristigupta12 <shristiguptawork@gmail.com>
2023-06-12 12:18:12 +0530

    modified

---
+++ my-calculator/src/App.js
@@ -0,0 +1,12 @@
+import Display from "./Components/display";
+import Keyboard from "./Components/keyboard";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Display />
+      <Keyboard />
+    </div>
+  );
+}
+
---
+++ my-calculator/src/Components/keyboard.js
@@ -0,0 +1,28 @@
+export default function Keyboard(){
+    return(
+        <div className="Keyboard">
+            <div className="numsWrapper">
+                <button>back</button>
+                <button>(</button>
+                <button>)</button>
+                <button>mod</button>
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button>/</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>*</button>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>-</button>
+                <button>0</button>
+                <button>.</button>
+                <button>+</button>
+                <button>=</button>
+            </div>
+        </div>
+    )
+}
